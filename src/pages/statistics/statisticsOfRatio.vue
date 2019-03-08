cff59051c79a0071c8efbd7f50a583880731f81a
张海泉 <haiquan.zhang@tcredit.com>
2019-03-08 18:46:31 +0800

    update

--- src/pages/statistics/statisticsOfRatio.vue
+++ src/pages/statistics/statisticsOfRatio.vue
@@ -22,18 +22,26 @@
     </el-form>
     <div class="main_bd">
       <el-table :data="tableData" stripe border style="width:100%" v-loading="loading">
-        <el-table-column prop="castId" label="机组" align="center" width="50px"></el-table-column>
-        <el-table-column prop="furnace" label="炉号" align="center" width="170px"></el-table-column>
-        <el-table-column prop="ribbonTypeName" label="材质" align="center" width="80px">
-        </el-table-column>
-        <el-table-column prop="ribbonWidth" label="规格" align="center" width="50px"></el-table-column>
         <el-table-column prop="caster" label="喷带手" align="center" width="70px"></el-table-column>
+        <el-table-column prop="totalHeatNum" label="炉次" align="center" width="50px"></el-table-column>
+        <el-table-column prop="lowHeatNum" label="低产炉次" align="center" width="80px"></el-table-column>
+        <el-table-column prop="zeroHeatNum" label="零产炉次" align="center" width="80px">
+        </el-table-column>
+        <el-table-column prop="lowAndZeroRatio" label="低产零产率" align="center" width="70px"></el-table-column>
+        <el-table-column prop="nozzleNum" label="喷嘴数" align="center" width="70px"></el-table-column>
+        <el-table-column prop="alloyTotalWeight" label="投入母合金(kg)" align="center" width="100px"></el-table-column>
         <el-table-column prop="rawWeight" label="大盘毛重(kg)" align="center" width="110px"></el-table-column>
-        <el-table-column prop="netWeight" label="倒卷净重(kg)" align="center" width="80px"></el-table-column>
-        <el-table-column prop="unqualifiedWeight" label="不合格重量(kg)" align="center" width="80px"></el-table-column>
+        <el-table-column prop="netWeight" label="倒卷净重(kg)" align="center" width="110px"></el-table-column>
+        <el-table-column prop="inPlanStoredWeight" label="计划内入库(kg)" align="center" width="90px"></el-table-column>
+        <el-table-column prop="outPlanStoredWeight" label="计划外入库(kg)" align="center" width="90px"></el-table-column>
         <el-table-column prop="totalStoredWeight" label="入库重量(kg)" align="center" width="110px"></el-table-column>
+        <el-table-column prop="unqualifiedWeight" label="不合格重量(kg)" align="center" width="80px"></el-table-column>
         <el-table-column prop="uselessRibbonWeight" label="废带(kg)" align="center" width="80px"></el-table-column>
-        <el-table-column prop="alloyTotalWeight" label="投入母合金(kg)" align="center" width="80px"></el-table-column>
+        <el-table-column prop="effectiveMeltRatio" label="有效过钢率" align="center" width="80px"></el-table-column>
+        <el-table-column prop="rollRatio" label="卷曲率" align="center" width="80px"></el-table-column>
+        <el-table-column prop="qualifiedRatio" label="合格率" align="center" width="80px"></el-table-column>
+        <el-table-column prop="totalRatio" label="直通率" align="center" width="80px"></el-table-column>
+        <el-table-column prop="inPlanRatio" label="订单合格率" align="center" width="80px"></el-table-column>
         <el-table-column label="质量等级分布" align="center" width="110px">
           <el-table-column label="A" prop="qualityOfA"></el-table-column>
           <el-table-column label="B" prop="qualityOfB"></el-table-column>
@@ -41,11 +49,6 @@
           <el-table-column label="D" prop="qualityOfD"></el-table-column>
           <el-table-column label="E" prop="qualityOfE"></el-table-column>
         </el-table-column>
-        <el-table-column prop="thinRibbonWeight" label="薄带(kg)" align="center" width="80px"></el-table-column>
-        <el-table-column prop="highFactorThinRibbonWeight" label="高叠片薄带(kg)" align="center" width="80px"></el-table-column>
-        <el-table-column prop="inPlanStoredWeight" label="计划内入库(kg)" align="center" width="90px"></el-table-column>
-        <el-table-column prop="outPlanStoredWeight" label="计划外入库(kg)" align="center" width="90px"></el-table-column>
-        <el-table-column prop="inPlanThickRibbonWeight" label="符合订单非薄带(kg)" align="center" width="100px"></el-table-column>
         <el-table-column prop="qualityOfGood" label="好(kg)" align="center" width="70px"></el-table-column>
         <el-table-column prop="qualityOfFine" label="良(kg)" align="center" width="70px"></el-table-column>
         <el-table-column prop="qualityOfNormal" label="中(kg)" align="center" width="70px"></el-table-column>
@@ -56,7 +59,6 @@
 
 <script>
 import urlmap from '@/utils/urlmap';
-import { mapState, mapActions } from 'vuex';
 
 export default {
   data() {
@@ -68,22 +70,12 @@ export default {
       tableData: []
     }
   },
-  computed: {
-    ...mapState([
-      'ribbonTypeList', 'ribbonWidthList'
-    ])
-  },
   created () {
     this.getTableData();
-    this.getRibbonTypeList();
-    this.getRibbonWidthList();
   },
   methods: {
-    ...mapActions([
-      'getRibbonTypeList', 'getRibbonWidthList'
-    ]),
     clickSearch() {
-      this.getTableData(params);
+      this.getTableData();
     },
     reset() {
       this.searchForm = { date: [] };
@@ -96,24 +88,27 @@ export default {
       };
       Object.assign(params, _params);
       this.$http('get', urlmap.queryStatisticsRatio, params).then(data => {
+        // data.list.forEach(item => {
+        //   const fromMeasure = item.fromMeasure;
+        //   item.totalStoredWeight = this.calcSum(fromMeasure, 'totalStoredWeight');
+        //   item.inPlanStoredWeight = this.calcSum(fromMeasure, 'inPlanStoredWeight');
+        //   item.outPlanStoredWeight = this.calcSum(fromMeasure, 'outPlanStoredWeight');
+        //   item.qualityOfA = this.calcSum(fromMeasure, 'qualityOfA');
+        //   item.qualityOfB = this.calcSum(fromMeasure, 'qualityOfB');
+        //   item.qualityOfC = this.calcSum(fromMeasure, 'qualityOfC');
+        //   item.qualityOfD = this.calcSum(fromMeasure, 'qualityOfD');
+        //   item.qualityOfE = this.calcSum(fromMeasure, 'qualityOfE');
+        //   item.thinRibbonWeight = this.calcSum(fromMeasure, 'thinRibbonWeight');
+        //   item.highFactorThinRibbonWeight = this.calcSum(fromMeasure, 'highFactorThinRibbonWeight');
+        //   item.inPlanThickRibbonWeight = this.calcSum(fromMeasure, 'inPlanThickRibbonWeight');
+        //   item.qualityOfGood = this.calcSum(fromMeasure, 'qualityOfGood');
+        //   item.qualityOfFine = this.calcSum(fromMeasure, 'qualityOfFine');
+        //   item.qualityOfNormal = this.calcSum(fromMeasure, 'qualityOfNormal');
+        //   item.netWeight = this.calcSum(fromMeasure, 'coilNetWeight');
+        //   item.unqualifiedWeight = item.netWeight - item.totalStoredWeight;
+        // });
         data.list.forEach(item => {
-          const fromMeasure = item.fromMeasure;
-          item.totalStoredWeight = this.calcSum(fromMeasure, 'totalStoredWeight');
-          item.inPlanStoredWeight = this.calcSum(fromMeasure, 'inPlanStoredWeight');
-          item.outPlanStoredWeight = this.calcSum(fromMeasure, 'outPlanStoredWeight');
-          item.qualityOfA = this.calcSum(fromMeasure, 'qualityOfA');
-          item.qualityOfB = this.calcSum(fromMeasure, 'qualityOfB');
-          item.qualityOfC = this.calcSum(fromMeasure, 'qualityOfC');
-          item.qualityOfD = this.calcSum(fromMeasure, 'qualityOfD');
-          item.qualityOfE = this.calcSum(fromMeasure, 'qualityOfE');
-          item.thinRibbonWeight = this.calcSum(fromMeasure, 'thinRibbonWeight');
-          item.highFactorThinRibbonWeight = this.calcSum(fromMeasure, 'highFactorThinRibbonWeight');
-          item.inPlanThickRibbonWeight = this.calcSum(fromMeasure, 'inPlanThickRibbonWeight');
-          item.qualityOfGood = this.calcSum(fromMeasure, 'qualityOfGood');
-          item.qualityOfFine = this.calcSum(fromMeasure, 'qualityOfFine');
-          item.qualityOfNormal = this.calcSum(fromMeasure, 'qualityOfNormal');
-          item.netWeight = this.calcSum(fromMeasure, 'coilNetWeight');
-          item.unqualifiedWeight = item.netWeight - item.totalStoredWeight;
+          item.caster = item._id;
         });
         this.tableData = data.list;
       }).catch((err) => {
---
+++ src/pages/statistics/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  mixins: [],
+  extends: {},
+  props: {},
+  watch: {},
+  components: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+<style lang='scss' scoped>
+</style>

d5058d7ddccfc4590cd5bece3f69fd93a972cb83
zhanghaiquan <zhqustb@163.com>
2019-10-03 19:01:03 +0800

    update

--- src/pages/clients/clients.vue
+++ src/pages/clients/clients.vue
@@ -1,79 +1,90 @@
 <template>
-<div>
-  <el-breadcrumb separator-class="el-icon-arrow-right" class="crumb">
-    <el-breadcrumb-item>客户管理</el-breadcrumb-item>
-  </el-breadcrumb>
-  <div class="main_bd">
-    <el-col class="table_hd">
-      <el-button type="primary" icon="el-icon-plus" @click="add">新增客户</el-button>
-    </el-col>
-    <el-table :data="clientsList" stripe border style="width:100%" v-loading="loading">
-      <el-table-column type="index" label="序号" align="center" width="200"></el-table-column>
-      <el-table-column prop="client" label="客户" align="center"></el-table-column>
-      <el-table-column prop="isFlat" label="是否必须平整" align="center">
-        <template slot-scope="scope">
-          <span>{{scope.row.isFlat === 1 ? '是' : '否'}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" align="center">
-        <template slot-scope="scope">
-          <el-button size="mini" type="primary" @click="edit(scope.row)">修改</el-button>
-          <el-button size="mini" type="danger" @click="del(scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right" class="crumb">
+      <el-breadcrumb-item>客户管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="main_bd">
+      <el-col class="table_hd">
+        <el-button type="primary" icon="el-icon-plus" @click="add">新增客户</el-button>
+      </el-col>
+      <el-table :data="clientsList" stripe border style="width:100%" v-loading="loading">
+        <el-table-column type="index" label="序号" align="center" width="200"></el-table-column>
+        <el-table-column prop="client" label="客户" align="center"></el-table-column>
+        <el-table-column prop="isFlat" label="是否必须平整" align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.isFlat === 1 ? '是' : '否'}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" align="center">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" @click="edit(scope.row)">修改</el-button>
+            <el-button size="mini" type="danger" @click="del(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <dialog-form
+      v-if="dialogVisible"
+      :dialogData="{ formType, dialogVisible, rowData}"
+      @close="closeHandler"
+      @submit="submitHandler"
+    ></dialog-form>
   </div>
-  <dialog-form v-if="dialogVisible" :dialogData="{ formType, dialogVisible, rowData}" @close="closeHandler" @submit="submitHandler"></dialog-form>
-</div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
-import dialogForm from './components/dialogForm';
-import urlmap from '@/utils/urlmap';
+import { mapState, mapActions } from "vuex";
+import dialogForm from "./components/dialogForm";
+import urlmap from "@/utils/urlmap";
 
 export default {
-  name: 'ribbon',
+  name: "ribbon",
   components: {
     dialogForm
   },
-  data () {
+  data() {
     return {
       dialogVisible: false,
-      formType: 'create',
+      formType: "create",
       rowData: {},
       tableData: [],
       loading: false
-    }
+    };
   },
   computed: {
-    ...mapState([ 'clientsList' ])
+    ...mapState(["clientsList"])
   },
-  created () {
+  created() {
     this.getClientsList();
   },
   methods: {
-    ...mapActions([ 'getClientsList' ]),
+    ...mapActions(["getClientsList"]),
     add() {
       this.dialogVisible = true;
-      this.formType = 'create';
+      this.formType = "create";
     },
     edit(row) {
       this.dialogVisible = true;
-      this.formType = 'edit';
+      this.formType = "edit";
       this.rowData = row;
     },
     del(row) {
       const { clientsId, client } = row;
-      this.$confirm(`删除后数据无法恢复，确定要删除 ${client} 吗？`, '提示', { confirmButtonText: '确定', cancelButtonText: '取消', type: 'warning'})
-      .then(() => {
-        this.$http('delete', urlmap.delClients, { clientsId }).then(data => {
-          this.getClientsList();
-        }).catch(err => {
-          console.log(err);
-        });
+      this.$confirm(`删除后数据无法恢复，确定要删除 ${client} 吗？`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       })
-      .catch(() => {});
+        .then(() => {
+          this.$http("delete", urlmap.delClients, { clientsId })
+            .then(data => {
+              this.getClientsList();
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        })
+        .catch(() => {});
     },
     closeHandler() {
       this.dialogVisible = false;
@@ -83,11 +94,10 @@ export default {
       this.getClientsList();
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
 
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import { http } from '@/utils/http';
-import urlmap from '@/utils/urlmap';
+import Vue from "vue";
+import Vuex from "vuex";
+import { http } from "@/utils/http";
+import urlmap from "@/utils/urlmap";
 
 Vue.use(Vuex);
 
@@ -44,79 +44,93 @@ export default new Vuex.Store({
     // 获取带材外观级别列表
     SET_APPERENCELEVELLIST(state, list) {
       state.appearenceList = list;
-    },
+    }
   },
   actions: {
     // 获取带材牌号列表
     getRibbonTypeList({ commit }) {
-      http('get', urlmap.queryRibbonType).then(data => {
-        // data.list && data.list.forEach((item, i) => {
-        //   item.ribbonTypeId = i + 1;
-        // });
-        commit('SET_RIBBONTYPELIST', data.list);
-      }).catch(err => {
-        console.log(err);
-      });
+      http("get", urlmap.queryRibbonType)
+        .then(data => {
+          // data.list && data.list.forEach((item, i) => {
+          //   item.ribbonTypeId = i + 1;
+          // });
+          commit("SET_RIBBONTYPELIST", data.list);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     // 获取带材规格列表
     getRibbonWidthList({ commit }) {
-      http('get', urlmap.queryRibbonWidth).then(data => {
-        // data.list && data.list.forEach(item => {
-        //   item.ribbonWidthId = i + 1;
-        // });
-        commit('SET_RIBBONWIDTHLIST', data.list);
-      }).catch(err => {
-        console.log(err);
-      });
+      http("get", urlmap.queryRibbonWidth)
+        .then(data => {
+          // data.list && data.list.forEach(item => {
+          //   item.ribbonWidthId = i + 1;
+          // });
+          commit("SET_RIBBONWIDTHLIST", data.list);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     // 获取带材厚度级别列表
     getRibbonThicknessLevelList({ commit }) {
-      http('get', urlmap.queryRibbonThicknessLevel).then(data => {
-        // data.list && data.list.forEach(item => {
-        //   item.ribbonThicknessLevelId = i + 1;
-        // });
-        commit('SET_RIBBONTHICKNESSLEVELLIST', data.list);
-      }).catch(err => {
-        console.log(err);
-      });
+      http("get", urlmap.queryRibbonThicknessLevel)
+        .then(data => {
+          // data.list && data.list.forEach(item => {
+          //   item.ribbonThicknessLevelId = i + 1;
+          // });
+          commit("SET_RIBBONTHICKNESSLEVELLIST", data.list);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     // 获取带材叠片级别列表
     getLaminationLevelList({ commit }) {
-      http('get', urlmap.queryLaminationLevel).then(data => {
-        // data.list && data.list.forEach(item => {
-        //   item.laminationLevelId = i + 1;
-        // });
-        commit('SET_LAMINATIONLEVELLIST', data.list);
-      }).catch(err => {
-        console.log(err);
-      });
+      http("get", urlmap.queryLaminationLevel)
+        .then(data => {
+          // data.list && data.list.forEach(item => {
+          //   item.laminationLevelId = i + 1;
+          // });
+          commit("SET_LAMINATIONLEVELLIST", data.list);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     // 获取带材韧性级别列表
     getRibbonToughnessLevelList({ commit }) {
-      http('get', urlmap.queryRibbonToughnessLevel).then(data => {
-        // data.list && data.list.forEach(item => {
-        //   item.ribbonToughnessLevelId = i + 1;
-        // });
-        commit('SET_RIBBONTOUGHNESSLEVELLIST', data.list);
-      }).catch(err => {
-        console.log(err);
-      });
+      http("get", urlmap.queryRibbonToughnessLevel)
+        .then(data => {
+          // data.list && data.list.forEach(item => {
+          //   item.ribbonToughnessLevelId = i + 1;
+          // });
+          commit("SET_RIBBONTOUGHNESSLEVELLIST", data.list);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     // 获取带材韧性级别列表
     getAppearenceLevelList({ commit }) {
-      http('get', urlmap.queryAppearenceLevel).then(data => {
-        commit('SET_APPERENCELEVELLIST', data.list);
-      }).catch(err => {
-        console.log(err);
-      });
+      http("get", urlmap.queryAppearenceLevel)
+        .then(data => {
+          commit("SET_APPERENCELEVELLIST", data.list);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     // 获取客户列表
     getClientsList({ commit }) {
-      http('get', urlmap.queryClients).then(data => {
-        commit('SET_CLIENTSLIST', data.list);
-      }).catch(err => {
-        console.log(err);
-      });
+      http("get", urlmap.queryClients)
+        .then(data => {
+          commit("SET_CLIENTSLIST", data.list);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 });
